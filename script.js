c4a932a893e1f2410884f6568173a1ec5cc45dbc
zeyneb-bounaim <zeynebbounaim@gmail.com>
2024-07-30 10:03:42 +0200

    add js

---
+++ script.js
@@ -0,0 +1,37 @@
+/*let name = prompt("c'est quoi ton blaze");*/
+/*alert("Si tu passes pas ton chat je vais venir dans ta maison le prendre moi-même.");
+console.log("what huh hin eh comment ca quoi " + name);
+alert("Si tu passes pas ton chat je vais venir dans ta maison le prendre moi-même.");
+console.log("HHUUUUUUUUH " + name + " cuicui");
+alert("Coucouu " + name + " !")
+console.log("Coucouu " + name + " ! Je compte voler tes parents, tes futurs enfants, tes voisins, tes profs, tes camarades, et ta maison si tu me donnes pas 40 chats. Maintenant.")
+let welcomeTitle = document.getElementById("welcomeTitle")
+
+welcomeTitle.innerText = "HIIIII POOKIE WOOKIE DOOKIE KOOKIE " + name;*/
+
+/*let  isOK = prompt("t'aimes les chats ?")
+
+if(isOK == "oui")
+{
+	console.log("J'aime bien cette personne")
+}
+else if(isOK == "Oui")
+{
+	console.log("J'aime bien cette personne")
+} 
+else if(isOK == "OUI")
+{
+	console.log("J'aime bien cette personne")
+} 
+else if(isOK == "non")
+{
+	console.log("Je n'aime pas cette personne")
+} 
+else if(isOK == "non")
+{
+	console.log("Je n'aime pas cette personne")
+} 
+else if(isOK == "NON")
+{
+	console.log("Je n'aime pas cette personne")
+}*/
